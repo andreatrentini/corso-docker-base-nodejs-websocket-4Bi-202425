064b5bad5e73711cb34ce6bd96812d08f1db4f3c
andreatrentini <55395467+andreatrentini@users.noreply.github.com>
2025-02-21 08:39:01 +0000

    feat: impostazioni iniziali dell'applicazione

---
+++ webapp/app.js
@@ -0,0 +1,10 @@
+// Dichiaro una costante che assume come valore il codice esportato dal package express
+const express = require('express');
+// Meccanismo per importare oggetti, funzioni, array dichiarati in altri file
+const config = require('./config');
+
+// app è una istanza di express, rappresenta la mia applicazione
+const app = express();
+
+
+
---
+++ webapp/config.js
@@ -0,0 +1,5 @@
+const config = {
+    port: 4300
+}
+
+module.exports = config;
